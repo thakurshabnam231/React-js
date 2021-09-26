dc78044dce6ea5f3fa0ff16bc110488443a370e6
shabnam rani <88382451+thakurshabnam231@users.noreply.github.com>
2021-09-26 20:48:07 +0530

    26/9/21

---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,41 @@
+
+import React,{useState} from 'react';
+import './ExpenseForm.css';
+const ExpenseForm = ()=>{
+    const[newItem,setNewItem]=useState();
+    const TitleEventlistener=()=>{
+        console.log(target.event.value)
+    };
+   const onSubmit=()=>{
+       setNewItem()
+console.log('submitted')
+    }
+    return(
+        
+        <form>
+            <div className='new-expense__controls'>
+                <div className='new-expense__controls'>
+                    
+         <label>Title</label>
+            <input type="text" onChange={TitleEventlistener}/>
+            </div>
+
+            <div className='new-expense__controls'>
+                       <label>Amount</label>
+            <input type="number" min= "0.01" step="0.01"/>
+            </div>
+              <div className='new-expense__controls'>
+              
+            <label>Date</label>
+           <input type='date' min ='2018-06-09' max='2022-09-01'/>
+           </div>
+           
+           <div className='new-expense__actions'>
+               <button onClick={onSubmit}> add expense</button>
+               
+           </div>
+           </div>
+         </form>
+    );
+};
+export default ExpenseForm;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,27 @@
+import React ,{useState} from 'react';
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  const[title,setTitle] = useState(props.title);
+  console.log('ExpensesItem evaluated by react');
+  const clickHandler=()=>{
+    setTitle('updated');
+    console.log(title);
+  }
+ 
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
+      </div>
+      <button onClick={clickHandler}>change title</button>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
